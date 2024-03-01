25cdbe7fd962fb180f6118a0bb9572e365634974
shahbaz <shahbazkhan19111@gmail.com>
2024-03-01 22:15:31 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import { Route, Routes } from "react-router-dom";
-import Products from './components/Products';
-
+import React from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import Products from "./components/Products";
+import About from "./components/About";
+import NavBar from './components/NavBar/NavBar'
 function App() {
+  const navigate = useNavigate();
+  const about = () => {
+    navigate("/aboutus");
+  };
   return (
     <div className="w-screen min-h-screen">
-
-      <div className=''>App</div>
-     
+      <NavBar/>
+      <button onClick={about}>click to About</button>
       <Routes>
-      <Route path="/prod" element={<Products/>} />
-      <Route path="/contact" element={<Products/>} />
-
-       </Routes>
-
-      </div>
-    
-
-  )
+        <Route path="/prod" element={<Products />} />
+        <Route path="/contact" element={<Products />} />
+        <Route path="/aboutus" element={<About />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
